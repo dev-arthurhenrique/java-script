3a749e5f9a0345139b593fa998f90015746a951b
dev-arthurhenrique <arthurnascimento2099@gmail.com>
2026-07-01 08:15:17 -0300

    atualizando o js

--- aula15-arrays/array3.js
+++ aula15-arrays/array3.js
@@ -1,3 +1,30 @@
 /*
 let cores = ['azul','vermelho', 'verde', 'amarelo', 'roxo']
 console.log(`Esse é o primeiro elemento: ${cores[0]}. Essa é o última cor: ${cores[4]}`)
+
+2.
+
+let bichos = ['cachorro', 'gato', 'coelho', 'papagaio']
+bichos.splice(1,1,'tigre')
+console.log(bichos)
+
+3
+
+
+let frutas = []
+frutas.push('Banana', 'Uva')
+frutas.unshift('Morango')
+frutas.splice(2,0,'Kiwi')
+console.log(frutas)
+
+4
+
+let numeros = [5,10,15,20,25,30]
+numeros.shift()
+numeros.pop()
+numeros.splice(2,1)
+console.log(numeros)
+*/
+let comidas = ['pizza', 'humburguer', 'sushi', 'lasanha', 'tacos']
+console.log(comidas.reverse())
+console.log(comidas.sort())
