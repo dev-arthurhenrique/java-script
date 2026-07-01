5d25ec7a97c77d326ac9542e22a0463a47591df9
dev-arthurhenrique <arthurnascimento2099@gmail.com>
2026-07-01 08:00:44 -0300

    atualizando js

--- aula15-arrays/array2.js
+++ aula15-arrays/array2.js
@@ -1,11 +1,10 @@
 let valores = [8,5,3,2,1,9]
-valores.push(7)
-valores.sort()
+console.log(`Valores em ordem: ${valores.sort()}`)
 /*
-for(cont = 0; cont < valores.length; cont++){
-    console.log(`O numero ${valores[cont]} está na posição ${cont}`)
+for(cont = 0; cont < valores.length; cont ++){
+    console.log(`O valor ${valores[cont]} está na posição ${cont}`)
 }
 */
 for(cont in valores){
-    console.log(`O número ${valores[cont]} está na posição ${cont}`)
+    console.log(`O valor ${valores[cont]} está na posição ${cont}`)
 }
--- aula15-arrays/array3.js
+++ aula15-arrays/array3.js
@@ -1,21 +1,3 @@
 /*
-let animais = ['Gato', 'Cachorro', 'Boi', 'Vaca', 'Texugo', 'Pato']
-console.log(`Nos temos ${animais.length} animais. São eles: ${animais}`)
-let cores = ['azul', 'vermelho', 'verde', 'amarelo', 'roxo']
-console.log(`A primeira cor é ${cores[0]}. A ultima cor é ${cores[4]}`)
-let bichos = ['cachorro', 'gato', 'coelho', 'papagaio']
-bichos[1] = 'Tigre'
-bichos.unshift('Formiga', 'cobra')
-console.log(`Esses são os bichos que temos: ${bichos}`)
-
-let frutas = []
-frutas.unshift('morango')
-frutas.push('banana','uva')
-frutas.splice(2,0,'kiwi')
-console.log(frutas)
-*/
-let numeros = [5,10,15,20,25,30]
-numeros.shift()
-numeros.pop()
-numeros.splice(2,1)
-console.log(numeros)
+let cores = ['azul','vermelho', 'verde', 'amarelo', 'roxo']
+console.log(`Esse é o primeiro elemento: ${cores[0]}. Essa é o última cor: ${cores[4]}`)
