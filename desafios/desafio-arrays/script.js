c2a9b81cabaa4a9a1d0db27d234f5519c1390e9e
dev-arthurhenrique <arthurnascimento2099@gmail.com>
2026-07-06 23:20:50 -0300

    atualizando projeto-analizador

--- desafios/desafio-arrays/script.js
+++ desafios/desafio-arrays/script.js
@@ -1,40 +1,42 @@
-//criando o evento de adicionar
-let adicionar = document.querySelector('input#adicionar')
+//disparando o evento para adicionar valores
+let adicionar = document.getElementById('adicionar')
 adicionar.addEventListener('click', a)
 let valores = []
 function a(){
-    let numero = document.querySelector('input#numero')
-    let n = Number(numero.value)
+    let num = document.getElementById('numero')
+    let n = Number(num.value)
     let seletor = document.getElementById('seletor')
-    let res = document.getElementById('res')
-    //validação de dados
-    if(numero.value.length == 0 || n < 1 || n > 100 || valores.indexOf(n) != -1){
-        alert('Número não está entre 1 e 100 ou já foi adicionado a lista')
+    if(num.value.length == 0 || n < 1 || n > 100 || valores.indexOf(n) != -1){
+        alert("Preencha o bloco com um número entre 1 e 100 e depois clique em 'Adicionar'. Além disso, você não pode adicionar valores repetidos")
+        num.focus()
+        num.value = ''
     }else{
         valores.push(n)
         let mostrar = document.createElement('option')
-        mostrar.text = `Valor ${n} adicionado`
+        mostrar.text = `O número ${n} foi adicionado a lista`
         seletor.appendChild(mostrar)
     }
 }
 let finalizar = document.getElementById('finalizar')
 finalizar.addEventListener('click', f)
 function f(){
-    let numero = document.querySelector('input#numero')
-    let n = Number(numero.value)
-    let seletor = document.getElementById('seletor')
-    let res = document.getElementById('res')
-    if(numero.value.length == 0){
-        alert('Adicione pelo menos um número antes de finalizar')
+    let num = document.getElementById('numero')
+    let n = Number(num.value)
+    if(num.value.length == 0){
+        alert("Preencha o bloco com um número entre 1 e 100 e depois clique em 'Adicionar'. Além disso, você não pode adicionar valores repetidos")
+        num.focus()
+        num.value = ''
     }else{
-        res.innerHTML = `Foram informados ${valores.length} número(s)<br>`
-        res.innerHTML += `O maior número digitado foi o ${Math.max(...valores)}<br>`
-        res.innerHTML += `O menor número digitado foi o ${Math.min(...valores)}<br>`
+        let res = document.getElementById('res')
+        res.innerHTML = `Ao todo, temos ${valores.length} números cadastrados. São eles em ordem crescente: ${valores.sort()}<br>`
+        res.innerHTML += `O maior valor é ${Math.max(...valores)}<br>`
+        res.innerHTML += `O menor valor é ${Math.min(...valores)}<br>`
         let soma = 0
-        for(cont = 0; cont < valores.length; cont++){
+        for(let cont = 0; cont < valores.length; cont++){
             soma += valores[cont]
         }
-        res.innerHTML += `A soma dos números digitados é ${soma}<br>`
-        res.innerHTML += `A média é ${(soma/valores.length).toFixed(2)}`
-    }      
+        res.innerHTML += `A soma entre os número é ${soma}<br>`
+        res.innerHTML += `A média entre os números é: ${(soma/valores.length).toFixed(2)}`
+    }
+    
 }
